e2859af8c3fb3f10ebf0fe67ca125160b36499fc
晴云孤魂 <hackkey@qq.com>
2022-03-26 17:20:20 +0800

    增强favorite-dirs函数

--- favorite-dirs.sh
+++ favorite-dirs.sh
@@ -7,6 +7,7 @@ favorite-dirs() {
 	Shell:documents
 	Shell:Sendto
 	Shell:Quick Launch
+	Shell:AppsFolder
 	Shell:AppData
 	%LocalAppdata%
 	Shell:startup
@@ -23,6 +24,30 @@ favorite-dirs() {
 	D:\Work\Documents
 EOF
 )
+	#追加paths/d函数使用的书签文件到本函数目录列表；
+	local mydirs="$mydirs"$'\n'"$(cat /v/bin/dirs.conf)"
+	local openerTool="explorer.exe"
+	while :;
+	do
+		#如果传入了$1参数，则对目录列表进行关键字搜索，而后仅列出匹配关键字的结果
+		if [ $# -eq 1 -a ! -z "$1" ];then
+			local mydirs=$(echo "$mydirs"|grep -i "$1")
+			break
+		elif [ $# -ge 1 ] && [[ "${1,,}" == "--open" ]];then #指定了额外程序替代资源管理器的情况
+			shift
+			if [ ! -z "$1" ] && [[ ! "$1" =~ ^\- ]];then
+				local openerTool="$1"
+				echo "使用 $openerTool 打开文件夹 ..."
+				shift
+			fi
+		elif [ $# -ge 1 ];then
+			local mydirs=$(echo "$mydirs"|grep $@)
+			break
+		else
+			break
+		fi
+	done
+	#echo "跳出参数解析..."
 	#输出选择菜单：
 	echo "$mydirs"|awk '{printf NR"): ";print}'
 	while :;
@@ -41,8 +66,22 @@ EOF
 				#local targetDir=$(echo "$mydirs"|awk "NR==$dirChoose"'{gsub(/^\s*/,"");print;exit}')
 				local targetDir=$(echo "$mydirs"|awk "NR==$1"'{gsub(/^\s*/,"");print;exit}')
 				if [ ! -z "$targetDir" ];then
-					echo "Open Dir $targetDir ..."
-					cmd /c explorer.exe "$targetDir "
+					#echo "Open Dir $targetDir ..."
+					#cmd /c explorer.exe "$targetDir"
+					if [[ "$targetDir" =~ ^[a-z]:\\ || "$targetDir" =~ ^/cygdrive/ || "$targetDir" =~ ^/ ]];then #常规路径模式下判断文件夹是否存在
+						if [ ! -e "$targetDir" ];then
+							#echo -e "targetDir not exist!\n==>$targetDir"
+							print_color 9 "targetDir not exist! ==> $targetDir"
+						else
+							echo "Open Dir $targetDir ..."
+							#cmd /c explorer.exe `cygpath -aw "$targetDir"`
+							cmd /c "$openerTool" `cygpath -aw "$targetDir"`
+						fi
+					else #适配Shell:模式或Windows原生环境变量模式；
+						echo "Open Dir $targetDir ..."
+						#cmd /c explorer.exe "$targetDir"
+						cmd /c "$openerTool" "$targetDir"
+					fi
 				else
 					echo "targetDir is empty!"
 				fi
