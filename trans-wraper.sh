4c65a3e84889fdae9217835843451a6fd4d107bf
晴云孤魂 <hackkey@qq.com>
2022-08-16 20:20:34 +0000

    add trans-wraper.sh.劫持trans命令，缺少详细参数时，自动判断英翻中还是中翻英，并新增多个快捷翻译的别名引用。 注意，依赖开源软件translate-shell (Github：https://github.com/soimort/translate-shell)
    Signed-off-by: 晴云孤魂 <hackkey@qq.com>

---
+++ trans-wraper.sh
@@ -0,0 +1,97 @@
+trans() {
+	#劫持trans同名命令，缺省详细参数时自动判断需要中翻英还是英翻中
+	#执行脚本：/v/bin/trans
+	# Github：https://github.com/soimort/translate-shell
+	
+	local header=$(cat<<EOF
+# trans 同名劫持函数
+# 执行脚本：/v/bin/trans
+# Github：https://github.com/soimort/translate-shell
+-----------
+# 你可以使用以下别名翻译为不同目标语言：
+`alias|grep 'alias trans-'`
+EOF
+)
+	[ $# -eq 0 ] && echo "$header" && /v/bin/trans --shell && return
+	local audioFile=""
+	local pointLang=0 #参数中是否指定了特定的翻译语言，1为已指定，0为未指定
+	local originOptions=( $@ )
+	while [ $# -gt 0 ];
+	do
+		if [[ "${1,,}" == "-download-audio-as" ]];then
+			audioFile="$2"
+		elif [[ "${1,,}" =~ ^: ]];then
+			pointLang=1
+		fi
+		shift
+	done
+	set -- ${originOptions[@]}
+	#判断参数起始字符是否是中文字符，从而决定中翻英还是英翻中
+	OLD_IFS=$IFS
+	IFS=$(echo -e "\n")
+	if [ $pointLang -eq 0 ] && [[ "${1,,}" =~ ^[^0-9a-z\-] ]];then
+		/v/bin/trans :en $*
+	else
+		/v/bin/trans $@
+	fi
+	IFS=$OLD_IFS
+	#是否自动播放语音文件（依赖于/v/bin/playaudio,实际调用程序cmdmp3）
+	#命令行播放音频第三方程序：cmdmp3（https://lawlessguy.wordpress.com/2015/06/27/update-to-a-command-line-mp3-player-for-windows/）
+	if [[ "$*" =~ "-download-audio-as" ]] && [ -f "$audioFile" ];then
+		print_color "播放语音文件..."
+		playaudio $audioFile
+		[ -f "$audioFile" ] && rm -vf $audioFile
+	fi
+}
+alias trans0=trans
+alias fy2=trans # <--- fy已指定其他程序(/usr/local/bin/fy.exe)
+alias fy=trans
+
+trans1() {
+	#翻译的同时自动下载语音文件到本地临时文件进行播放
+	#alias trans1='trans -download-audio-as /tmp/textaudio.mp3' # <--- alias由于传参顺序不对，暂时弃用...
+	local transTarget="" #<--翻译的目标语言
+	#如果参数$1指定了翻译的目标语种，则调换参数顺序，以便为trans1创建多语种别名，同时保证trans函数良好执行
+	#eg： trans1 邵氏影院 :fr
+	#	  等效于
+	#	  trans1 :fr 邵氏影院   （指定翻译目标语种为法语）
+	#-------------------
+	# 别名调用示例（翻译为法语并朗读）：trans-fa 邵氏影院
+	if [[ "$1" =~ ^\: ]];then  
+		#echo "需要调换参数顺序..."
+		transTarget="$1" && shift
+	fi
+	set -- "$@" "$transTarget" # <--- 注意 $@ 加双引号，否则英文句子会被分成多个参数传递
+	#set -x
+	OLD_IFS=$IFS
+	IFS=$(echo -e "\n")
+	trans $@ -download-audio-as /tmp/textaudio.mp3
+	IFS=$OLD_IFS
+	#set +x
+}
+alias fy3=trans1
+#翻译支持的语言：https://github.com/soimort/translate-shell/wiki/Languages
+alias trans-fa='trans1 :fr' #翻译为法语
+alias trans-de='trans1 :de' #翻译为德语
+alias trans-en='trans1 :en' #翻译为英语
+alias trans-es='trans1 :es' #翻译为西班牙语
+alias trans-spa=trans-es
+alias trans-it='trans1 :it' #翻译为意大利语
+alias trans-yi=trans-it
+alias trans-ja='trans1 :ja' #翻译为日语
+alias trans-ri=trans-ja
+alias trans-ko='trans1 :ko' #翻译为韩语
+alias trans-han=trans-ko
+alias trans-ms='trans1 :ms' #翻译为马来语
+alias trans-malai=trans-ms
+alias trans-my='trans1 :my' #翻译为缅甸语
+alias trans-mian=trans-my
+alias trans-vi='trans1 :vi' #翻译为越南语
+alias trans-yn=trans-vi
+alias trans-th='trans1 :th' #翻译为泰语
+alias trans-tai=trans-th
+alias trans-tl='trans1 :tl' #翻译为菲律宾
+alias trans-fei=trans-tl
+alias trans-zh='trans1 :zh-CN' #翻译为简体中文
+alias trans-zhft='trans1 :zh-TW' #翻译为繁体中文
+alias trans-yue='trans1 :yue' #翻译为粤语
