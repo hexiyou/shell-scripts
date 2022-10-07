4d2b1dcdea3b5c3b56f61f14fdbad01a13d5acef
晴云孤魂 <hackkey@qq.com>
2022-10-08 03:36:44 +0800

    优化killsshps函数，支持管道传入参数跳过询问，直接终止进程

--- killsshps.sh
+++ killsshps.sh
@@ -4,7 +4,7 @@ killsshps() {
 	# $1 --> 可指定主机名称或IP地址；
 	local host
 	local hostIP
-	local defaultJumpHost="host01" #默认经常使用的跳板机
+	local defaultJumpHost="kunming" #默认经常使用的跳板机
 	
 	[ -z "$1" ] && echo "请指定SSH主机名或IP地址!" && return
 	[[ "$1" =~ ^[0-9]{1,3}[0-9\.]+$ ]] && hostIP="$1" || host="$1"
@@ -16,12 +16,20 @@ killsshps() {
 		print_color 40 "没有找到主机 \"$host\" !"
 		return
 	fi
-	findremoteip "$hostIP" ssh
+	if [ -t 0 ];then
+		findremoteip "$hostIP" ssh
+	else
+		findremoteip "$hostIP" ssh </dev/stdin
+	fi
 	local retCode=$?
 	
 	if [[ ! -z "$host" && ! -z "$defaultJumpHost" && "${host,,}" != "$defaultJumpHost" && $retCode != 0 ]];then #如果没有找到链接进程，可能使用了跳板机作为隧道进程，尝试终止kunming主机进程
 		print_color 40 "没有找到 $host 主机相关 SSH 进程..."
 		print_color 40 "查询是否有 $defaultJumpHost 主机进程，请确认 $host 主机是否使用了 $defaultJumpHost 作为跳板机？"
-		killsshps $defaultJumpHost
+		if [ -t 0 ];then
+			killsshps $defaultJumpHost
+		else
+			killsshps $defaultJumpHost </dev/stdin
+		fi
 	fi
 }
